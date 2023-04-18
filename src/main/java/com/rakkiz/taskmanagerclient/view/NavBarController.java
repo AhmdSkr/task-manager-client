39b3d60fc1a8f81ab1d2c9b1c9009230b7232523
janasabeh <janasabeh56@gmail.com>
2023-04-18 19:23:34 +0300

    Continued TaskSack, navbar + Made filer

--- src/main/java/com/rakkiz/taskmanagerclient/view/NavBarController.java
+++ src/main/java/com/rakkiz/taskmanagerclient/view/NavBarController.java
@@ -1,4 +1,36 @@
 package com.rakkiz.taskmanagerclient.view;
 
+import com.rakkiz.taskmanagerclient.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NavBarController {
+    @FXML
+    private Hyperlink taskSack;
+    @FXML
+    private Hyperlink pomodoro;
+
+    @FXML
+    private void onTaskSackClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/task-sack.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) taskSack.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void onPomodoroClick() throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/pomodoro.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) pomodoro.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
