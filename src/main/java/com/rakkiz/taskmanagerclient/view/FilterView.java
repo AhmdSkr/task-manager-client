19169e41e70372c961bf24b7c0f981327f893d0d
janasabeh <janasabeh56@gmail.com>
2023-04-20 21:18:50 +0300

    Edited Views, Tasks, TaskDetails

---
+++ src/main/java/com/rakkiz/taskmanagerclient/view/FilterView.java
@@ -0,0 +1,8 @@
+package com.rakkiz.taskmanagerclient.view;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+public class FilterView {
+}
--- src/main/java/com/rakkiz/taskmanagerclient/view/TaskView.java
+++ src/main/java/com/rakkiz/taskmanagerclient/view/TaskView.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
@@ -13,11 +14,21 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class TaskController {
+public class TaskView {
+    private int id;
+    @FXML
+    private Label title, description;
+
+    public void setTitle(String realTitle) {
+        title.setText(realTitle);
+    }
+
+    public void setDescription(String realDescription) {
+        description.setText(realDescription);
+    }
 
     @FXML
     private AnchorPane task;
-
     @FXML
     private void goToDetails() throws IOException {
         // create the popup stage
@@ -26,16 +37,31 @@ public class TaskController {
         popupStage.setResizable(false);
         popupStage.initModality(Modality.APPLICATION_MODAL);
 
-
         // create the popup scene from the FXML file
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/task-details.fxml"));
         Parent popupContent = (Parent) fxmlLoader.load();
         Scene popupScene = new Scene(popupContent);
 
+        // set stage and id of popup stage
+        TaskDetailsView controller = fxmlLoader.getController();
+        controller.setStage(popupStage);
+        controller.setId(this.id);
+
+        // put stage position on screen
+        popupStage.setX(300);
+        popupStage.setY(150);
 
         // set the scene of the popup stage
         popupStage.setScene(popupScene);
 
+        // blur the background stage
+        BoxBlur blur = new BoxBlur(10, 10, 3);
+        Parent parentRoot = task.getScene().getRoot();
+        parentRoot.setEffect(blur);
+
+        popupStage.setOnHidden(event -> {
+            parentRoot.setEffect(null);
+        });
         // show the popup
         popupStage.showAndWait();
     }
