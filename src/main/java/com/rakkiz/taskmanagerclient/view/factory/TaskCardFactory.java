a5089b4edce5b588fb7e284db8374f14e2f05a3c
yaacoub <azzam.yaacoub@gmail.com>
2023-04-24 18:36:33 +0300

    added TaskCardFactory

---
+++ src/main/java/com/rakkiz/taskmanagerclient/view/factory/TaskCardFactory.java
@@ -0,0 +1,31 @@
+package com.rakkiz.taskmanagerclient.view.factory;
+
+import com.rakkiz.taskmanagerclient.data.model.Task;
+import com.rakkiz.taskmanagerclient.view.TaskView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
+
+public class TaskCardFactory {
+    private FXMLLoader loader;
+    public TaskCardFactory(){
+        loader = new FXMLLoader();
+        loader.setLocation(TaskCardFactory.class.getResource("task.fxml"));
+    }
+
+    public AnchorPane create(Task task) {
+        AnchorPane taskCard = null;
+        try {
+
+            taskCard = loader.load();
+//            TaskView taskCardController = loader.getController();
+//            taskCardController.setTitle(task.getName());
+//            taskCardController.setDescription(task.getDescription());
+            taskCard.lookup("#title");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return taskCard;
+    }
+
+}
