672bdbbeecd45087a2eb6628171c48d8b11ddb8b
yaacoub <azzam.yaacoub@gmail.com>
2023-04-29 21:09:37 +0300

    added pomodoro timer controller and model

---
+++ src/main/java/com/rakkiz/taskmanagerclient/controller/PomodoroTimerController.java
@@ -0,0 +1,114 @@
+package com.rakkiz.taskmanagerclient.controller;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import com.rakkiz.taskmanagerclient.data.model.PomodoroTimerModel;
+
+public class PomodoroTimerController {
+    @FXML
+    private Label time;
+
+    @FXML
+    private Label pomCycles;
+
+    @FXML
+    private VBox Vbox;
+
+    @FXML
+    private ToggleButton SP;
+
+    BackgroundFill backgroundFillRed = new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY);
+    BackgroundFill backgroundFillBlue = new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY);
+    BackgroundFill backgroundFillGreen = new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY);
+
+    // Create a Background object with the BackgroundFill object
+    Background backgroundRed = new Background(backgroundFillRed);
+    Background backgroundBlue = new Background(backgroundFillBlue);
+    Background backgroundGreen = new Background(backgroundFillGreen);
+
+
+    PomodoroTimerModel model;
+
+    String timeString;
+
+    @FXML
+    public void initialize() {
+        model = new PomodoroTimerModel();
+        model.setWorkState();
+        time.setText(model.getOriginalWorkTimeInSeconds());
+        Vbox.setStyle("-fx-background-color: rgba(248, 196, 200, 0.5); -fx-background-radius: 50%;");
+        Vbox.setBackground(backgroundRed);
+        pomCycles.setText(""+model.getCycleCount());
+
+        // Listener for workTimeSeconds
+        model.workTimeInSecondsProperty().addListener((obs, oldCount, newCount) -> {
+            int minutes = newCount.intValue() / 60;
+            int seconds = newCount.intValue() % 60;
+            timeString= String.format("%02d:%02d", minutes, seconds);
+            time.setText(timeString);
+            if (newCount.intValue() == 0){
+
+                Vbox.setStyle("-fx-background-color: rgba(199, 215, 226, 0.5); -fx-background-radius: 50%;");
+                if(model.getCycleCount()!=1) {
+                    time.setText("" + model.getOriginalBreakTimeInSeconds());
+                }
+            }
+        });
+        // Listener for breakTimeSeconds
+        model.breakTimeInSecondsProperty().addListener((obs, oldCount, newCount) -> {
+            int minutes = newCount.intValue() / 60;
+            int seconds = newCount.intValue() % 60;
+            timeString= String.format("%02d:%02d", minutes, seconds);
+            time.setText(timeString);
+            if (newCount.intValue() == 0){
+                Vbox.setStyle("-fx-background-color: rgba(248, 196, 200, 0.5); -fx-background-radius: 50%;");
+            }
+        });
+        // Listener for cycleCount
+        model.cycleCountProperty().addListener((obs, oldCount, newCount) ->{
+            pomCycles.setText(""+newCount.intValue());
+            if(newCount.intValue() == 0){
+                Vbox.setStyle("-fx-background-color: rgba(188, 226, 186, 0.5); -fx-background-radius: 50%;");
+                time.setText("DONE!");
+                SP.setDisable(true);
+                SP.setVisible(false);
+            }
+        });
+
+        SP.setOnAction(event -> {
+            if(SP.isSelected()){
+                startTimer();
+                SP.setText("pause");
+            }else {
+                stopTimer();
+                SP.setText("start");
+            }
+        });
+
+    }
+
+
+
+    @FXML
+    private void startTimer() {
+        switch (model.getCurrentState()) {
+            case WORK -> model.startWorkTimer();
+            case BREAK -> model.startBreakTimer();
+        }
+    }
+
+    @FXML
+    private void stopTimer() {
+        switch (model.getCurrentState()) {
+            case WORK -> model.stopWorkTimer();
+            case BREAK -> model.stopBreakTimer();
+        }
+    }
+}
---
+++ src/main/java/com/rakkiz/taskmanagerclient/data/model/PomodoroTimerModel.java
@@ -0,0 +1,200 @@
+package com.rakkiz.taskmanagerclient.data.model;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.util.Duration;
+
+
+public class PomodoroTimerModel{
+
+
+    private Timeline workTimer;
+    private Timeline breakTimer;
+    private final int originalWorkTimeInSeconds = 1 * 10; // 25 minutes
+    private final int originalBreakTimeInSeconds = 1 * 5; // 5 minutes
+    private int originalCycleCount = 2;
+
+
+
+    //TEST
+    private IntegerProperty workTimeInSeconds = new SimpleIntegerProperty(originalWorkTimeInSeconds);
+
+
+
+    private IntegerProperty breakTimeInSeconds = new SimpleIntegerProperty(originalBreakTimeInSeconds);
+
+
+
+    private IntegerProperty cycleCount = new SimpleIntegerProperty(originalCycleCount);
+
+
+
+    public int getWorkTimeInSeconds() {
+        return workTimeInSeconds.get();
+    }
+
+    public IntegerProperty workTimeInSecondsProperty() {
+        return workTimeInSeconds;
+    }
+
+
+    public int getBreakTimeInSeconds() {
+        return breakTimeInSeconds.get();
+    }
+
+    public IntegerProperty breakTimeInSecondsProperty() {
+        return breakTimeInSeconds;
+    }
+
+    public IntegerProperty cycleCountProperty(){
+        return cycleCount;
+    }
+
+
+    public void decrementWorkTime() {
+        workTimeInSeconds.set(workTimeInSeconds.get() - 1);
+    }
+
+    public void decrementBreakTime() {
+        breakTimeInSeconds.set(breakTimeInSeconds.get() - 1);
+    }
+
+    public void decrementCycleCount() { cycleCount.set(cycleCount.get() - 1);}
+
+
+    //TEST
+
+    public enum TimerState {WORK, BREAK};
+    private TimerState currentState;
+
+
+    public void setWorkState(){
+        currentState = TimerState.WORK;
+    }
+    public void setBreakState(){
+        currentState = TimerState.BREAK;
+    }
+
+    public TimerState getCurrentState(){
+        return currentState;
+    }
+
+    public String getOriginalWorkTimeInSeconds(){
+        return formatTime(originalWorkTimeInSeconds);
+    }
+
+//    public void setOriginalWorkTimeInSeconds(int seconds){
+//        this.workTimeInSeconds = seconds;
+//    }
+
+
+    public String getOriginalBreakTimeInSeconds(){
+        return formatTime(originalBreakTimeInSeconds);
+    }
+
+//    public void setOriginalBreakTimeInSeconds(int seconds){
+//        this.breakTimeInSeconds = seconds;
+//    }
+
+    public int getCycleCount(){
+        return cycleCount.get();
+    }
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void startWorkTimer() {
+        workTimer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            decrementWorkTime();
+            if (getWorkTimeInSeconds() == 0) {
+                stopWorkTimer();
+                resetBreakTimer();
+                startBreakTimer();
+                //originalCycleCount--;
+                decrementCycleCount();
+                if (cycleCount.get() == 0) {
+                    stopTimers();
+                }
+            }
+        }));
+        workTimer.setCycleCount(Timeline.INDEFINITE);
+        workTimer.play();
+        currentState = TimerState.WORK;
+
+    }
+
+    public void stopWorkTimer() {
+        workTimer.stop();
+        currentState = TimerState.WORK;
+    }
+
+    public void startBreakTimer() {
+        breakTimer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            decrementBreakTime();
+            if (getBreakTimeInSeconds() == 0) {
+                stopBreakTimer();
+                resetWorkTimer();
+                startWorkTimer();
+            }
+        }));
+        breakTimer.setCycleCount(Timeline.INDEFINITE);
+        breakTimer.play();
+        currentState = TimerState.BREAK;
+    }
+
+    public void stopBreakTimer() {
+        breakTimer.stop();
+        currentState = TimerState.BREAK;
+    }
+
+    private void resetWorkTimer() {
+        workTimeInSeconds.set(originalWorkTimeInSeconds);
+    }
+
+    private void resetBreakTimer() {
+        breakTimeInSeconds.set(originalBreakTimeInSeconds);
+    }
+
+    private void stopTimers() {
+        if (workTimer != null) {
+            workTimer.stop();
+        }
+        if (breakTimer != null) {
+            breakTimer.stop();
+        }
+        //cycleCount.set(originalCycleCount);
+        //resetWorkTimer();
+        //resetBreakTimer();
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//    public Background getBackgroundColor(String color) {
+//        switch (color) {
+//            case "red":
+//                return backgroundRed;
+//            //    break;
+//            case "blue":
+//                return backgroundBlue;
+//             //   break;
+//            case "green":
+//                return backgroundGreen;
+//              //  break;
+//        }
+//        return backgroundRed;
+//    }
+
+
+    private String formatTime(int timeInSeconds) {
+        int minutes = timeInSeconds / 60;
+        int seconds = timeInSeconds % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+}
