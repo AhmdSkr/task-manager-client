1c4084cf2166c6ffcaf63f414ce4a32c7ecc590c
AhmdSkr <116917027+AhmdSkr@users.noreply.github.com>
2024-02-20 23:43:29 +0200

    Updated dependencies and exports
    - Migrating to JavaFx 21
    - Added lombok to project
    - Updated module info: removed exports (will be added again if needed)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,15 @@
 module com.rakkiz.management.task.client {
+    requires static lombok;
+
     requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
 
-    opens com.rakkiz.management.task.client to javafx.fxml;
-    exports com.rakkiz.management.task.client;
-    exports com.rakkiz.management.task.client.data.model;
-    exports com.rakkiz.management.task.client.controller;
+    opens com.rakkiz.management.task.client to javafx.fxml, javafx.graphics;
     opens com.rakkiz.management.task.client.controller to javafx.fxml;
-    exports com.rakkiz.management.task.client.view.strategy;
     opens com.rakkiz.management.task.client.view.strategy to javafx.fxml;
-    exports com.rakkiz.management.task.client.view.strategy.date;
     opens com.rakkiz.management.task.client.view.strategy.date to javafx.fxml;
-    exports com.rakkiz.management.task.client.view.strategy.duration;
     opens com.rakkiz.management.task.client.view.strategy.duration to javafx.fxml;
-    exports com.rakkiz.management.task.client.view.strategy.type;
     opens com.rakkiz.management.task.client.view.strategy.type to javafx.fxml;
 }
